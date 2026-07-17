69e7278e563fd0cf2284bdaeca994f0fb1859404
Rodolpho Rosa da Silva <rodolphorosadasilva@Laptop-de-Rodolpho.local>
2026-07-17 10:07:38 -0300

    deleted frontend-legacy folder

--- frontend-legacy/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env",        // converte ES2020+ para ES5
-    "@babel/preset-react",      // compila JSX
-    "@babel/preset-typescript"  // entende .ts e .tsx
-  ],
-};
